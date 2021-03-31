4da5c12bfa3b81efca1ffafaf5c39b8573363a48
Geraldo Hendriks <Geraldohendriks@gmail.com>
2021-03-31 16:13:03 +0200

    used cases instead of if-else statements

--- commands/steenpapierschaar.js
+++ commands/steenpapierschaar.js
@@ -1,64 +1,72 @@
 const random = require('random')
 
 module.exports = {
-	name: 'sps',
-	description: 'Speel steen papier schaar met Ventius',
-	execute(message, args) {
+    name: 'sps',
+    description: 'Speel steen papier schaar met Ventius',
+    execute(message, args) {
         let sps = ["steen", "papier", "Schaar", "steen", "papier", "Schaar", "steen", "papier", "Schaar"];
         var numRandom = random.int((min = 0), (max = 8));
         var answer = sps[numRandom];
         const win = "je hebt gewonnen";
         const loss = "je hebt verloren";
         const tie = "het is gelijkspel";
-         
-        
 
 
-        if (args[0] === "steen" && answer === "schaar") {
-            message.channel.send(`${args[0]} VS ${answer}`);
-            message.channel.send(win);
-        }
 
-        else if (args[0] === "steen" && answer === "papier") {
-            message.channel.send(`${args[0]} VS ${answer}`);
-            message.channel.send(loss);
-        }
+        switch (args[0], answer) {
+            case "steen", "schaar":
+                message.channel.send(`${args[0]} VS ${answer}`);
+                message.channel.send(win);
+                break;
 
-        else if (args[0] === "steen" && answer === "steen") {
-            message.channel.send(`${args[0]} VS ${answer}`);
-            message.channel.send(tie);
-        }
+            case "steen", "papier":
+                message.channel.send(`${args[0]} VS ${answer}`);
+                message.channel.send(loss);
+                break;
 
-        else if (args[0] === "papier" && answer === "steen") {
-            message.channel.send(`${args[0]} VS ${answer}`);
-            message.channel.send(win);
-        }
-        else if (args[0] === "papier" && answer === "schaar") {
-            message.channel.send(`${args[0]} VS ${answer}`);
-            message.channel.send(loss);
-        }
-        else if (args[0] === "papier" && answer === "papier") {
-            message.channel.send(`${args[0]} VS ${answer}`);
-            message.channel.send(tie);
-        }
-        else if (args[0] === "schaar" && answer === "papier") {
-            message.channel.send(`${args[0]} VS ${answer}`);
-            message.channel.send(win);
-        }
-        else if (args[0] === "schaar" && answer === "steen") {
-            message.channel.send(`${args[0]} VS ${answer}`);
-            message.channel.send(loss);
-        }
-        else if (args[0] === "schaar" && answer === "schaar") {
-            message.channel.send(`${args[0]} VS ${answer}`);
-            message.channel.send(tie);
-        }
-        else if (args[0] === "pistool") {
-            message.channel.send(`${args[0]} VS ${answer}`);
-            message.channel.send(`${win} want een pistool wint altijd!`);
-        }
-        else {
-            message.channel.send('Verkeerde invoer. sps <steen, papier, schaar>')
-        }
-	},
+            case "steen", "steen":
+                message.channel.send(`${args[0]} VS ${answer}`);
+                message.channel.send(tie);
+                break;
+            
+            case "papier", "steen":
+                message.channel.send(`${args[0]} VS ${answer}`);
+                message.channel.send(win);
+                break;
+
+            case "papier", "schaar":
+                message.channel.send(`${args[0]} VS ${answer}`);
+                message.channel.send(loss);
+                break;
+
+            case "papier", "papier":
+                message.channel.send(`${args[0]} VS ${answer}`);
+                message.channel.send(tie);
+                break;
+
+            case "schaar", "papier":
+                message.channel.send(`${args[0]} VS ${answer}`);
+                message.channel.send(win);
+                break;
+            
+            case "schaar", "steen":
+                message.channel.send(`${args[0]} VS ${answer}`);
+                message.channel.send(loss);
+                break;
+            
+            case "schaar", "schaar":
+                message.channel.send(`${args[0]} VS ${answer}`);
+                message.channel.send(tie);
+                break;
+
+            case "pistool":
+                message.channel.send(`${args[0]} VS ${answer}`);
+                message.channel.send(`${win} want een pistool wint altijd!`);
+                break;
+
+            default:
+                message.channel.send('Verkeerde invoer. sps <steen, papier, schaar>')
+                break;
+        };
+    },
 };
