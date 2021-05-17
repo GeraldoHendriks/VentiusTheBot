2e5ead1b60adfab222ef31bd755edc3dcffa08c8
Geraldo Hendriks <Geraldohendriks@gmail.com>
2021-05-17 12:33:44 +0200

    Fixed RPS and added a clear command

--- commands/steenpapierschaar.js
+++ commands/steenpapierschaar.js
@@ -1,72 +1,36 @@
 const random = require('random')
 
 module.exports = {
-    name: 'sps',
-    description: 'Speel steen papier schaar met Ventius',
+    name: 'rps',
+    description: 'Play <Rock Paper Scissors> with Ventius',
     execute(message, args) {
-        let sps = ["steen", "papier", "Schaar", "steen", "papier", "Schaar", "steen", "papier", "Schaar"];
-        var numRandom = random.int((min = 0), (max = 8));
-        var answer = sps[numRandom];
-        const win = "je hebt gewonnen";
-        const loss = "je hebt verloren";
-        const tie = "het is gelijkspel";
+        const acceptedReplies = ['rock', 'paper', 'scissors'];
+        const random = Math.floor((Math.random() * acceptedReplies.length));
+        const result = acceptedReplies[random];
 
+        const choice = args[0];
+        if (!choice) return message.channel.send(`How to play: \`-rps <rock|paper|scissors>\``);
+        if (!acceptedReplies.includes(choice)) return message.channel.send(`Only these responses are accepted: \`${acceptedReplies.join(', ')}\``);
 
-
-        switch (args[0], answer) {
-            case "steen", "schaar":
-                message.channel.send(`${args[0]} VS ${answer}`);
-                message.channel.send(win);
-                break;
-
-            case "steen", "papier":
-                message.channel.send(`${args[0]} VS ${answer}`);
-                message.channel.send(loss);
-                break;
-
-            case "steen", "steen":
-                message.channel.send(`${args[0]} VS ${answer}`);
-                message.channel.send(tie);
-                break;
-            
-            case "papier", "steen":
-                message.channel.send(`${args[0]} VS ${answer}`);
-                message.channel.send(win);
-                break;
-
-            case "papier", "schaar":
-                message.channel.send(`${args[0]} VS ${answer}`);
-                message.channel.send(loss);
-                break;
-
-            case "papier", "papier":
-                message.channel.send(`${args[0]} VS ${answer}`);
-                message.channel.send(tie);
-                break;
-
-            case "schaar", "papier":
-                message.channel.send(`${args[0]} VS ${answer}`);
-                message.channel.send(win);
-                break;
-            
-            case "schaar", "steen":
-                message.channel.send(`${args[0]} VS ${answer}`);
-                message.channel.send(loss);
-                break;
-            
-            case "schaar", "schaar":
-                message.channel.send(`${args[0]} VS ${answer}`);
-                message.channel.send(tie);
-                break;
-
-            case "pistool":
-                message.channel.send(`${args[0]} VS ${answer}`);
-                message.channel.send(`${win} want een pistool wint altijd!`);
-                break;
-
-            default:
-                message.channel.send('Verkeerde invoer. sps <steen, papier, schaar>')
-                break;
-        };
+        console.log('Bot Result:', result);
+        if (result === choice) return message.reply("It's a tie! We had the same choice.");
+        
+        switch (choice) {
+            case 'rock': {
+                if (result === 'paper') return message.reply('I won!');
+                else return message.reply('You won!');
+            }
+            case 'paper': {
+                if (result === 'scissors') return message.reply('I won!');
+                else return message.reply('You won!');        
+            }
+            case 'scissors': {
+                if (result === 'rock') return message.reply('I won!');
+                else return message.reply('You won!');
+            }
+            default: {
+                return message.channel.send(`Only these responses are accepted: \`${acceptedReplies.join(', ')}\``);
+            }
+        }
     },
 };
